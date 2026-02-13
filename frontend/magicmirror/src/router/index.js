35f92a31cbb36184cf0f37a3391c0ef53d8938c6
Levin <levin.schmid@schindler.com>
2026-02-13 14:50:01 +0100

    feat: replace HomeView with DashboardView and implement file listing

--- frontend/magicmirror/src/router/index.js
+++ frontend/magicmirror/src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '../views/DashboardView.vue'
 import DisplayView from '../views/DisplayView.vue'
 import UploadView from '../views/UploadView.vue'
 
---
+++ frontend/magicmirror/src/views/DashboardView.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { ref } from 'vue';
+
+const files = ref([
+    {
+        id: 1,
+        name: 'eins',
+        volume: '100gb',
+        active: true
+    },
+    {
+        id: 2,
+        name: 'zwei',
+        volume: '222gb',
+        active: false
+    }
+])
+</script>
+<template>
+<h1>Dashboard</h1>
+<div>
+    <table>
+        <tr>
+            <th>FileName</th>
+            <th>Volume</th>
+            <th>active</th>
+        </tr>
+        <tr v-for="file in files" :key="file" class="board">
+            <th>{{ file.name }}</th>
+            <th>{{ file.volume }}</th>
+            <th><input type="checkbox" :checked="file.active" @change="changedActive(key)"></th>
+        </tr>
+    </table>
+</div>
+</template>
+<style scoped>
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+</style>
